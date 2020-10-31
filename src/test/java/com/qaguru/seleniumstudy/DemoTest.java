e3f059fa1c5cdd300b0e360748a7aceb23300b7c
Bettin-QAGuru-Student <bettincash@gmail.com>
2020-10-31 15:35:42 -0400

    Added Test for searching a product. Closes #2

--- src/test/java/com/qaguru/seleniumstudy/DemoTest.java
+++ src/test/java/com/qaguru/seleniumstudy/DemoTest.java
@@ -1,7 +1,9 @@
 package com.qaguru.seleniumstudy;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,7 +12,7 @@ public class DemoTest {
     private final String baseUrl = "https://www.homeandstuff.com/";
 
     @Test
-    public void testHomePageTitle(){
+    public void testHomePageTitle() throws InterruptedException {
         System.out.println("Testing the Home Page Title");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -20,7 +22,21 @@ public class DemoTest {
         String expTitle = "Furniture, Kitchen, Dining Room, Entertainment, Bedroom Sets, Outdoor, Fireplaces";
         String actTitle = driver.getTitle();
         Assert.assertEquals(actTitle, expTitle, "Title is wrong");
-        //Thread.sleep(1000);
+        Thread.sleep(1000);
+        driver.quit();
+
+    }
+
+    @Test
+    public void testSearchingForAProduct() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get(baseUrl);
+        WebElement txtSearch = driver.findElement(By.name("search_field"));
+        txtSearch.sendKeys("table");
+        WebElement btnSearch = driver.findElement(By.xpath("//*[@id=\"search\"]/form/div/span/input"));
+        btnSearch.click();
+        WebElement lnkResult = driver.findElement(By.linkText("Atlantic Height Adjustable Standing Desk Converter Large"));
         driver.quit();
 
     }
